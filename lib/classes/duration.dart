29026086f8a80d99f3867274168afea73ac6f2cf
Al <lary.Sene@gmail.com>
2022-11-10 22:43:41 -0400

    LSDuration

---
+++ lib/classes/duration.dart
@@ -0,0 +1,10 @@
+class LSDuration extends Duration {
+  Duration duration;
+  LSDuration({
+    required this.duration,
+  });
+  int toMap() => inMilliseconds;
+  factory LSDuration.fromDuration(Duration m) => LSDuration(duration: m);
+  factory LSDuration.fromMap(dynamic m) =>
+      LSDuration(duration: Duration(milliseconds: m));
+}
---
+++ lib/extension/duration.extension.dart
@@ -0,0 +1,10 @@
+extension DurationExtension on Duration {
+  String _twoDigits(int n) => n.toString().padLeft(2, "0");
+
+  String get toHH => _twoDigits(inHours);
+  String get remainingMinutes => _twoDigits(inMinutes.remainder(60));
+  String get remainingSeconds => _twoDigits(inSeconds.remainder(60));
+  String get toHHMM => "$toHH:$remainingMinutes";
+  String get toHHMMSS => "$toHHMM:$remainingSeconds";
+
+}
