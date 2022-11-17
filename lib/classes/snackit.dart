82cf25621a082be0ac117e4f6bc641fe097c5947
Al <lary.Sene@gmail.com>
2022-11-16 23:55:09 -0400

    snackit

---
+++ lib/classes/snackit.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Snackit {
+  static info(String msg, BuildContext context) =>
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Row(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0),
+              child: Icon(
+                Icons.info_outline,
+                color: Colors.blueAccent,
+              ),
+            ),
+            Text(msg),
+          ],
+        ),
+        behavior: SnackBarBehavior.floating,
+        elevation: 5.0,
+      ));
+
+  static sucess(String msg, BuildContext context) =>
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        backgroundColor: Colors.green,
+        content: Row(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0),
+              child: Icon(
+                Icons.verified,
+              ),
+            ),
+            Text(
+              msg,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+        behavior: SnackBarBehavior.floating,
+        elevation: 5.0,
+      ));
+
+  static error(String msg, BuildContext context) =>
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        backgroundColor: Colors.redAccent,
+        content: Row(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0),
+              child: Icon(
+                Icons.cancel,
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                msg,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+        behavior: SnackBarBehavior.floating,
+        elevation: 5.0,
+      ));
+}
